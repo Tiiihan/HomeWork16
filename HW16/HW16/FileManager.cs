2366e640594dcd18c597d3c97c9694216fd961f0
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-07-25 14:30:27 +0300

    Resolved task

---
+++ HW16/HW16/FileManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW16
+{
+	internal class FileManager
+	{
+		private string sourcePath = @"";
+		private string destinationPath = @"";
+
+		public string SourcePath { get; set; }
+		public string DestinationPath { get; set; }
+
+		public string ReadAllText()
+		{
+			if (IsSourceFileExists(SourcePath))
+				return File.ReadAllText(SourcePath, Encoding.UTF8);
+			else
+			{
+				Console.WriteLine("File does not exist");
+				return null;
+			}
+		}
+		public void CopyText()
+		{ 
+			string content = ReadAllText();
+
+			if (content != null)
+			{
+				File.WriteAllText(DestinationPath, content);
+				Console.WriteLine("File was successfuly copied");
+			}
+			else Console.WriteLine("File could not be read");
+		}
+		public static (string source, string destination) GetPathFromUser()
+		{
+			(string source, string destination) paths;
+
+			Console.Write("Enter path from: ");
+			paths.source = Console.ReadLine();
+
+			Console.Write("Enter path to: ");
+			paths.destination = Console.ReadLine();
+
+			return paths;
+		}
+		public void ValidPath()
+		{
+			bool isValid = false;
+			(string source, string destination) paths = GetPathFromUser();
+
+			while (!isValid)
+			{
+				if (IsPathEmpty(paths) || ArePathsEqual(paths) || !IsSourceFileExists(paths.source))
+				{
+					Console.WriteLine("Invalid input");
+					paths = GetPathFromUser();
+				}
+				else
+				{
+					isValid = true;
+					SourcePath = paths.source;
+					DestinationPath = paths.destination;
+				}
+			}
+		}
+		public bool IsPathEmpty((string source, string destination) paths)
+		{
+			bool IsPathEmpty = false;
+
+			if (string.IsNullOrEmpty(paths.source) || string.IsNullOrEmpty(paths.destination))
+				IsPathEmpty = true;
+			return IsPathEmpty;
+		}
+
+		public bool ArePathsEqual((string source, string destination) paths)
+		{
+			bool IsPathsEqual = false;
+
+			if (paths.destination == paths.source)
+				IsPathsEqual = true;
+			return IsPathsEqual;
+		}
+
+		public bool IsSourceFileExists(string source) => File.Exists(source);
+	}
+}
--- HW16/HW16/Program.cs
+++ HW16/HW16/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Ваша мета - створити програму, яка копіює вміст одного текстового файлу в інший. 
+//Користувач повинен ввести шлях до вихідного файлу та шлях до файлу, в який потрібно скопіювати дані. 
+//Після виконання копіювання виведіть повідомлення про успішне завершення.
+
+using HW16;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+FileManager fileManager = new FileManager();
+
+fileManager.ValidPath();
+
+try
+{
+	fileManager.CopyText();
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
